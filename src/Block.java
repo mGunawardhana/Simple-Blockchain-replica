fabfc214a0f4ca3c299e6491ebba95240a4bf655
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-08-21 23:27:21 +0530

    creating and implementing hashcode and toString

--- src/Block.java
+++ src/Block.java
@@ -11,13 +11,65 @@ public class Block {
     private String data;
     private int Hash;
 
-    public Block(String data,int previousHash){
-        this.data=data;
-        this.previousHash=previousHash;
+    public Block(String data, int previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.Hash = Arrays.hashCode(new Integer[]{data.hashCode(), previousHash});
+    }
 
-        /* ---- creating blockChain ---- */
+    public Block() {
+        super();
+    }
 
-        this.Hash = Arrays.hashCode(new Integer[]{data.hashCode(),previousHash});
+    public int getPreviousHash() {
+        return previousHash;
+    }
 
+    public void setPreviousHash(int previousHash) {
+        this.previousHash = previousHash;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getHash() {
+        return Hash;
+    }
+
+    public void setHash(int hash) {
+        Hash = hash;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    @Override
+    public String toString() {
+        return "Block{" +
+                " previousHash = " + previousHash + " " +
+                ", Hash = " + Hash + " " +
+                '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,64 @@
  * what's app - 071 - 9043372
  */
 
+import com.sun.org.apache.xml.internal.security.algorithms.implementations.SignatureDSA;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+
+    static List<Block> blockChainList = new ArrayList<>();
+
     public static void main(String[] args) {
 
-        List<Block> blockChainList = new ArrayList<>();
 
         /* ---- Introducing genesis Block ---- */
-        Block genesis = new Block("mGunawardhana's BlockChain",0);
+        Block genesis = new Block("mGunawardhana's BlockChain", 0);
         blockChainList.add(genesis);
 
         /* ---- Introducing first block ---- */
         //TODO: check out this line if you have an any error
-        Block blockA = new Block("Block One here",blockChainList.get(0).hashCode());
+        Block blockA = new Block("Block One here", blockChainList.get(0).hashCode());
         blockChainList.add(blockA);
 
-        Block blockB = new Block("Block two here",blockChainList.get(blockChainList.size()-1).hashCode());
+        Block blockB = new Block("Block two here", blockChainList.get(blockChainList.size() - 1).hashCode());
         blockChainList.add(blockB);
 
-        Block blockC = new Block("Block three here",blockChainList.get(blockChainList.size()-1).hashCode());
+        Block blockC = new Block("Block three here", blockChainList.get(blockChainList.size() - 1).hashCode());
         blockChainList.add(blockC);
 
-        Block blockD = new Block("Block four here",blockChainList.get(blockChainList.size()-1).hashCode());
+        Block blockD = new Block("Block four here", blockChainList.get(blockChainList.size() - 1).hashCode());
         blockChainList.add(blockD);
 
+        Block blockE = new Block("Block five here", blockChainList.get(blockChainList.size() - 1).hashCode());
+        blockChainList.add(blockE);
+
+        Block blockF = new Block("Block six here", blockChainList.get(blockChainList.size() - 1).hashCode());
+        blockChainList.add(blockF);
+
+        Block blockG = new Block("Block Seven here", blockChainList.get(blockChainList.size() - 1).hashCode());
+        blockChainList.add(blockG);
+
+        Block blockH = new Block("Block Eight here", blockChainList.get(blockChainList.size() - 1).hashCode());
+        blockChainList.add(blockH);
+
+        Block blockI = new Block("Block Nine here", blockChainList.get(blockChainList.size() - 1).hashCode());
+        blockChainList.add(blockI);
+
+        Block blockJ = new Block("Block Ten here", blockChainList.get(blockChainList.size() - 1).hashCode());
+        blockChainList.add(blockJ);
 
 
+        System.out.println("===============================================================");
+        System.out.println("|        - Welcome to mGunawardhana's Blockchain Sample -     |");
+        System.out.println("===============================================================");
 
+        for (Block block : blockChainList) {
+            System.out.println("|    " + block + "   ");
+        }
+
+        System.out.println("===============================================================");
 
     }
 }
