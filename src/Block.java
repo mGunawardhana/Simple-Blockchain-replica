8ababe838b84dd63eb5534a960012a4ee35390da
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-08-21 22:40:35 +0530

    introducing lis for store these blocks

--- src/Block.java
+++ src/Block.java
@@ -4,12 +4,20 @@
  * what's app - 071 - 9043372
  */
 
+import java.util.Arrays;
+
 public class Block {
     private int previousHash;
     private String data;
     private int Hash;
 
     public Block(String data,int previousHash){
+        this.data=data;
+        this.previousHash=previousHash;
+
+        /* ---- creating blockChain ---- */
+
+        this.Hash = Arrays.hashCode(new Integer[]{data.hashCode(),previousHash});
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,15 @@
  * what's app - 071 - 9043372
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        List<Block> blockChainList = new ArrayList<>();
+
+
 
     }
 }
