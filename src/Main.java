18735c35d2be6e32d0fb66c26bc343be01509d27
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-08-21 22:28:53 +0530

    introducing Main and Block Class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/*
+ * Developed by - mGunawardhana
+ * Contact email - mrgunawardhana27368@gmail.com
+ * what's app - 071 - 9043372
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
